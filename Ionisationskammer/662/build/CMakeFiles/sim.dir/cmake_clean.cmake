c87e7810cb78e2f8896b777902ad2b6686cdd666
uhorbach <73596823+uhorbach@users.noreply.github.com>
2021-05-11 12:24:27 +0200

    Add files via upload

---
+++ Ionisationskammer/662/build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/main.cc.o"
+  "CMakeFiles/sim.dir/src/UserDefinedActionInitialization.cc.o"
+  "CMakeFiles/sim.dir/src/UserDefinedDetectorConstruction.cc.o"
+  "CMakeFiles/sim.dir/src/UserDefinedPrimaryGeneratorAction.cc.o"
+  "sim.pdb"
+  "sim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
